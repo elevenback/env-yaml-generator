4e81c254a947cc4094178e63ad9cbe0e5c11f7ab
HANATANI Takuma <mail@potato4d.me>
2019-10-05 11:02:41 +0900

    chore: Improve Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.ts'
+  ],
+  coveragePathIgnorePatterns: [
+    '\\.spec\\.ts'
+  ],
   testPathIgnorePatterns: [
     './lib/'
   ]
