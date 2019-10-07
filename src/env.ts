ae9398ecad8f45410f2a160497977ae68068d318
motitaiyaki <7887955+motitaiyaki@users.noreply.github.com>
2019-10-07 12:56:11 +0900

    fix: use reduce instead of fromEntries

--- src/env.ts
+++ src/env.ts
@@ -30,9 +30,14 @@ export function getFilteredEnvironments(
   filter: string
 ): Environments {
   const regex = new RegExp(filter)
-  return Object.fromEntries(
-    Object.entries(environments).filter(([k, v]) => regex.exec(k))
-  )
+  return Object.entries(environments)
+    .filter(([k, v]) => regex.exec(k))
+    .reduce((before, after) => {
+      return {
+        ...before,
+        [after[0]]: after[1]
+      }
+    }, {})
 }
 
 export function convertAppEngineStyleYamlFromEnvironments(
